6dce8313b34984dd27e65ba98364bd95ccdfdf9a
Moses Kurniawan <moseskurniawan2002@gmail.com>
2022-11-16 22:35:57 +0700

    luwe masseh

---
+++ server.js
@@ -0,0 +1,68 @@
+// //VARIABEL
+// var x = 5
+// //console.log(nama)
+// function cobaVariabel(a) {
+//     
+
+//ARRAY
+var buah = ["mangga", "salak", "melon"]
+//console.log(buah[2])
+
+var warna = {tua: "biru", muda: "merah"}
+//console.log(warna)
+
+var x = 5
+var y = 6
+var z = 5 + 6
+//console.log(++x)
+
+
+
+function angka(a) {
+            if (!(a<0 && a<-5)) {
+            return a = "negatif";
+        }
+        else if (a>0) {
+            return a = "positif"
+        }
+        else {
+            return a = "boong"
+        }
+    }
+    console.log(angka(-6))
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var x = "moshe"
+// let y = 15
+// var z = true
+
+// //console.log(++y)
+// function cobaLogika(a) {
+//     if (a<0) {
+//         return a = "bilangan negatif" ;
+//     }
+//     else if (a >2 && a>8) {
+//         return a = "bilangan positif" ;
+//     }
+//     else {
+//         return a= "boong" ;
+//     }
+// }
+// //console.log(y + 1)
+// //Array
+// var buah = ["Apel", "Melon", "Anggur"]
+// //console.log(buah[1])
+// //JSON
+// let uwu = {nama: "Moses"}
+// console.log(nama)
