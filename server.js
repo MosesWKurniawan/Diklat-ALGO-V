9999d48475ef61efdf2fbd5eab86aa63f352b74b
unknown <mn.fikqih03@gmail.com>
2022-11-17 21:48:23 +0700

    penambahan file

---
+++ server.js
@@ -0,0 +1,20 @@
+function diskon (x) {
+let musimpandemik = (x * 30) /100
+return musimpandemik
+}
+let sale = diskon (20000)
+//console.log (sale) // output:6000
+class car {
+    constructor (fiqih, 2022) {
+        this.name = fiqih;
+        this.year = 2022 ;
+    }
+    age (x) {
+        return x -this.2022;
+    }
+}
+let date = new date ();
+let year = date.get.FullYear();
+let myCar = new myCar ("toyota",2022) ;
+console.log(" my car is" + myCar.age(year) + "years old")
+
